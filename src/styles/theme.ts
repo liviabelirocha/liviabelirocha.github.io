88adfaccd59fb59f2d292b2972618d41c14c8771
liviabelirocha <liviabelirocha@outlook.com>
2021-07-29 18:24:28 -0300

    Configuring project

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+	colors: {
+		backgroundPrimary: '#191919',
+		backgroundSecondary: '#2c2c31',
+		textPrimary: '#bb1cff',
+		textSecondary: '#ffffff'
+	}
+}
+
+export default theme
