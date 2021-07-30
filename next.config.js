ac4cb559076da5ea5737325b7211b2e284254b37
liviabelirocha <liviabelirocha@outlook.com>
2021-07-29 22:37:11 -0300

    CDN

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withImages = require('next-images')
 
+const isProd = process.env.NODE_ENV === 'production'
+
+
 module.exports = withImages({
 	fileExtensions: ['jpg', 'jpeg', 'png', 'gif'],
-	esModule: true
+	esModule: true,
+	assetPrefix: isProd ? 'https://cdn.statically.io/gh/liviabelirocha/https://liviabelirocha.github.io/' : '',
 })
