4abefb5f89429b7094ebe88fe4f34060f6721a33
Lívia Belizário <e-livia.rocha@allos.co>
2024-09-25 18:30:12 -0300

    chore: deploy i guess

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-const withImages = require('next-images')
-
-const isProd = process.env.NODE_ENV === 'production'
+const withImages = require("next-images");
 
 module.exports = withImages({
-	fileExtensions: ['jpg', 'jpeg', 'png', 'gif'],
-	esModule: true
-})
+	fileExtensions: ["jpg", "jpeg", "png", "gif"],
+	esModule: true,
+	output: "export",
+});
